149c9457f329498991ff9db4c3b3b5d0f30cd1b1
AudioStakes <daisuke.sato@enishi-tech.com>
2022-12-19 16:07:00 +0900

    Update index.ts to return a quote ramdomly

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { Hono } from "hono";
+
+const QUOTES = [
+  {
+    quote: "The only way to do great work is to love what you do.",
+    by: "Steve Jobs",
+  },
+  {
+    quote: "The best way to predict the future is to invent it.",
+    by: "Alan Kay",
+  },
+  {
+    quote:
+      "There are only two hard things in Computer Science: cache invalidation and naming things.",
+    by: "Phil Karlton",
+  },
+];
+
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hono!!"));
+app.get("/", (c) => {
+  const quote = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+  return c.json(quote);
+});
 
 export default app;
